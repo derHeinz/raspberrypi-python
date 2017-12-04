7374218dd6c9ebfacf2289f871151ca59b04d05d
derHeinz <heinzserfas@gmx.de>
2017-12-04 20:39:17 +0100

    add ability for continuous wake on lan

---
+++ wakeonlan.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Library to use wake on lan.
+Call me like this wakeonlan.py <Openhabitem> <Mac-for-wakeonla>.
+"""
+
+import sys
+import subprocess
+import postopenhab
+
+if len(sys.argv) < 2 or None == sys.argv[1]:
+	print("OFF")
+else:
+	item = sys.argv[1]
+	mac = sys.argv[2]
+	macWithColon = mac.replace("-", ":")
+	
+	val = postopenhab.get_switch_value_from_openhab(item)
+	result = None
+	if (val):
+		# wakeonlan -p 9 00:11:22:33:44:55
+		call_result = subprocess.call(["wakeonlan", "-p", "9", macWithColon], stdout=subprocess.PIPE)
+		result = "ON"
+	else:
+		reult = "OFF"
+	print(result)
