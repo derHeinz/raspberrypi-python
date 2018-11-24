7d0d1448d11bd41d7f72da199f3c6037df89433a
derHeinz <heinzserfas@gmx.de>
2018-11-24 20:36:10 +0100

    switch to mac address resolution

--- search_name.py
+++ search_name.py
@@ -6,7 +6,7 @@ import re
 import os
 
 def find_ips(mac_or_name):
-	res = subprocess.check_output(["arp", "-a"])
+	res = subprocess.check_output(["arp", "-an"])
 	lines = res.split(b'\n')
 	ips = []
 
@@ -16,12 +16,19 @@ def find_ips(mac_or_name):
 			bla = bla[1].rsplit(")", 1)
 			ip = bla[0]
 			ips.append(ip)
+			
 	return ips
 	
 def check_online(ip):
 	if subprocess.call(["ping", "-c1", "-w1", "-t1", ip], stdout=open(os.devnull, 'w')) == 0:
 		return True
 	return False
+	
+def find_online_ip_by_mac(mac):
+	# uncomment mac
+	if ("-" in mac):
+		mac = mac.replace("-", ":")
+	return find_online_ip(mac)
 	
 def find_online_ip(mac_or_name):
 	for ip in find_ips(mac_or_name):
