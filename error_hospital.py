dd5c7104afa7cc03e122aa6bb4f5ab80707ed492
derHeinz <heinzserfas@gmx.de>
2018-11-25 11:20:55 +0100

    adding error hospital

---
+++ error_hospital.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import postopenhab
+import datetime
+import traceback
+
+errorlog_file = "errors.log"
+
+def write_to_errorlog(text):
+	f = open(errorlog_file, 'a')
+	f.write(text)
+	f.write('\n')
+	f.close()
+
+def error_happened(exc):
+	tb = traceback.format_exc()
+	now = datetime.datetime.now().isoformat()
+	write_to_errorlog(str(now) + " " + str(tb))
