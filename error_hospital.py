a19ba22a7b1bf71a7c5604c97542e07b8c6f6ef6
derHeinz <heinzserfas@gmx.de>
2019-08-17 23:00:24 +0200

    error hospital to write error

--- error_hospital.py
+++ error_hospital.py
@@ -7,12 +7,12 @@ import traceback
 errorlog_file = "errors.log"
 
 def write_to_errorlog(text):
+    now = datetime.datetime.now().isoformat()
     f = open(errorlog_file, 'a')
-    f.write(text)
+    f.write(str(now) + " ERROR " + text)
     f.write('\n')
     f.close()
 
 def error_happened(exc):
     tb = traceback.format_exc()
-    now = datetime.datetime.now().isoformat()
-    write_to_errorlog(str(now) + " " + str(tb))
+    write_to_errorlog(str(tb))
