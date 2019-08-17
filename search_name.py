5252db561e9fb68491f58a62b510c21e8a4f3ebc
derHeinz <heinzserfas@gmx.de>
2019-08-17 23:01:03 +0200

    search_name refactored to search either mac name or ip

--- search_name.py
+++ search_name.py
@@ -2,11 +2,77 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-import re
 import os
 
-def find_ips(mac_or_name):
-    res = subprocess.check_output(["arp", "-an"]).decode("utf-8")
+def find_online_ip(mac_name_ip):
+    if (_validate_ip(mac_name_ip)):
+        # it's a valid ip - so just return
+        return mac_name_ip
+    if (_validate_mac(mac_name_ip)):
+        # it's a valid mac
+        mac = _uniform_mac(mac_name_ip)
+        for ip in _find_ips_by_mac(mac):
+            if _check_online(ip):
+                return ip
+       # throw error otherwise
+    
+    # it must be a name
+    name = mac_name_ip
+    for ip in _find_ips_by_name(name):
+        if _check_online(ip):
+            return ip
+
+def find_online_ip_by_mac(mac):
+   mac = _uniform_mac(mac)
+    
+   for ip in _find_ips_by_mac(mac):
+       if _check_online(ip):
+           return ip
+
+def _validate_mac(potential_mac):
+    mac = _uniform_mac(potential_mac)
+    parts = mac.split(":")
+    # check length of 6
+    if len(parts) != 6:
+        return False
+    # check each part to be alphanumerical hex
+    for part in parts:
+        try:
+            int(part, 16)
+        except ValueError:
+            return False
+    return True
+    
+def _validate_ip(potential_ip):
+    parts = potential_ip.split('.')
+    if len(parts) != 4:
+        return False
+    for part in parts:
+        if not part.isdigit():
+            return False
+        i = int(part)
+        if i < 0 or i > 255:
+            return False
+    return True
+    
+def _uniform_mac(mac):
+    if ("-" in mac):
+        mac = mac.replace("-", ":")
+    mac = mac.lower()
+    return mac
+    
+def _find_ips_by_mac(mac):
+    return _find_ips(mac, False)
+    
+def _find_ips_by_name(name):
+    return _find_ips(name, True)
+
+def _find_ips(mac_or_name, with_names):
+    arp_params = "-a"
+    if (not with_names):
+        # do not show host names
+        arp_params += "n"
+    res = subprocess.check_output(["arp", arp_params]).decode("utf-8")
     lines = res.split('\n')
     ips = []
 
@@ -18,21 +84,9 @@ def find_ips(mac_or_name):
             ips.append(ip)
             
     return ips
-    
-def check_online(ip):
+
+def _check_online(ip):
     if subprocess.call(["ping", "-c1", "-w1", "-t1", ip], stdout=open(os.devnull, 'w')) == 0:
         return True
     return False
-    
-def find_online_ip_by_mac(mac):
-    # uncomment mac
-    if ("-" in mac):
-        mac = mac.replace("-", ":")
-    mac = mac.lower()
-    return find_online_ip(mac)
-    
-def find_online_ip(mac_or_name):
-    for ip in find_ips(mac_or_name):
-        if check_online(ip):
-            return ip
                 
