c89278a175ca1839cc5c82fb83230de2583dc62a
derHeinz <heinzserfas@gmx.de>
2020-11-29 20:42:03 +0100

    calendarparse updated, remove dependency to postopenhab

--- calendarparse.py
+++ calendarparse.py
@@ -4,12 +4,11 @@
 from ics import Calendar, Event
 from ics.timeline import Timeline
 import arrow
-from . postopenhab import post_value_to_openhab
 
-def complete_filename(filename):
+def _complete_filename(filename):
     return "/etc/openhab2/calendar/" + filename
 
-def find_events(day=None, file_or_list=None):
+def _find_events(day=None, file_or_list=None):
     result_list = []
 
     # default is today
@@ -23,7 +22,7 @@ def find_events(day=None, file_or_list=None):
         files = file_or_list
         
     for file in files:
-        cal = Calendar(open(complete_filename(file)).read())
+        cal = Calendar(open(_complete_filename(file)).read())
         tl = Timeline(cal)
 
         # only one event can happen
@@ -36,26 +35,12 @@ def find_events(day=None, file_or_list=None):
 
     return result_list
     
-def find_events_str(day=None, file_or_list=None):
-    events_list = find_events(day=day, file_or_list=file_or_list)
+def _find_events_str(day, file_or_list):
+    events_list = _find_events(day=day, file_or_list=file_or_list)
     return ', '.join(events_list)
     
-def print_events(day=None, file_or_list=None):
-    print(find_events_str(day=day, file_or_list=file_or_list))
-    
-def post_event_to_openhab(day, itemname, file_or_list):
-    text = find_events_str(day=day, file_or_list=file_or_list)
-
-    if (text is not None):
-        post_value_to_openhab(itemname, text)
-    else:
-        post_value_to_openhab(itemname, "")
-        
-def post_today_event_to_openhab(itemname, file_or_list):
-    day = arrow.utcnow()    
-    post_event_to_openhab(day, itemname, file_or_list)
-
-def post_tomorrow_event_to_openhab(itemname, file_or_list):
+def get_event_str(dayshift, file_or_list):
     day = arrow.utcnow()
-    day = day.shift(days=+1)
-    post_event_to_openhab(day, itemname, file_or_list)
+    day = day.shift(days=dayshift)
+    return _find_events_str(day, file_or_list)
+    
